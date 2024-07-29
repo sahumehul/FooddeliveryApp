77c2071ab581fd8887a9729b793d77022f6bc8da
Mehul Sahu <mehulyashams@gmail.com>
2024-07-30 00:29:58 +0530

    ft(validator added)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const userRouter = express.Router();
-const User = require("../model/User")
+const User = require("../model/User");
+const {body, validationResult} = require("express-validator");
 
-userRouter.post("/createuser", async(req,res)=>{
+userRouter.post("/createuser", body("email","Incorrect Email").isEmail(),body("password","Length should be 5").isLength({min: 5}), async(req,res)=>{
     const {name, email, password, location} = req.body;
-
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()})
+    }
     const userData = new User({
         name, email,password,location
     })
