fc6ca266dfd96350ab4ab02d5c2bf8c2ca94a368
Mehul Sahu <mehulyashams@gmail.com>
2024-08-03 23:35:25 +0530

    ft(login UI added)

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import React,{useState} from "react";
+import axios from "axios";
+import { Link,useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  });
 
-export default Login
+  const navigate= useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/login",
+        {
+          email: credentials.email,
+          password: credentials.password
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      navigate("/")
+      const json = response.data;
+      console.log(json);
+
+      if (!json.success) {
+        alert("Enter valid credentials");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while submitting the form");
+    }
+  };
+
+  const onChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+  return (
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              onChange={onChange}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              onChange={onChange}
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="m-3 btn btn-success">
+            Submit
+          </button>
+          <Link to="/createuser" className="m-3 btn btn-danger">
+            Don't have an account?
+          </Link>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
