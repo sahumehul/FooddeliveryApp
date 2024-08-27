36b0cd9ae64c9f865c93843748795ffb4b669921
Mehul Sahu <mehulyashams@gmail.com>
2024-08-27 23:20:17 +0530

    ft(icons added in footer)

--- src/componants/Footer.js
+++ src/componants/Footer.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p className="fw-bold">&copy; 2024 Your Company. All rights reserved.</p>
         <div className="social-links">
-          <a href="https://www.linkedin.com/in/mehul-sahu-4352181b2/" target="_blank" rel="noopener noreferrer">Linkedin</a>
-          <a href="https://www.instagram.com/mehul.mg.16/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a
+            href="https://www.linkedin.com/in/mehul-sahu-4352181b2/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i
+              className="ri-linkedin-box-line"
+              style={{ fontSize: "19px" }}
+            ></i>
+          </a>
+          <a
+            href="https://www.instagram.com/mehul.mg.16/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="ri-instagram-line" style={{ fontSize: "19px" }}></i>
+          </a>
         </div>
       </div>
     </footer>
